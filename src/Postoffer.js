422ad7e855682c3d621d94d3536fbdf53eaea46d
Quantumminded <lukkino31@gmail.com>
2022-12-16 15:32:13 +0100

    adding all pages to src

---
+++ src/Postoffer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Postoffer() {
+  return (
+    <div>Postoffer</div>
+  )
+}
+
+export default Postoffer
---
+++ src/TransaltionForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TransaltionForm() {
+  return (
+    <div>TransaltionForm</div>
+  )
+}
+
+export default TransaltionForm
