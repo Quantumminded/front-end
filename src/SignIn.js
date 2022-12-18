cdd6061a42b90b766a64a3b91b497c9698f8e7b7
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-18 03:53:50 +0100

    import font

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,17 +1,26 @@
 import React from 'react'
-
+import styled from "styled-components";
+import {Small,Body,Highlight,Subtitle,Title,Headings} from './Style/StyledTypography'
 function SignIn() {
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+`
+  // function onSignIn(googleUser) {
+  //   var profile = googleUser.getBasicProfile();
+  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //   console.log('Name: ' + profile.getName());
+  //   console.log('Image URL: ' + profile.getImageUrl());
+  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // }
   return (
     <>
     <div>SignIn</div>
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    <Container>
+      <Title>SignIn</Title>
+    <div className="g-signin2" data-onsuccess="onSignIn"></div>
+    </Container>
     </>
   )
 }
---
+++ src/Style/fonts.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto+Slab&family=Rubik:wght@700&display=swap');
+/* body {
+    margin: 0;
+    padding: 0;
+    background: teal;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  } */
+`;
