4acc2d58433f46e4b33d113ea84e23c08dfd2a50
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-19 15:53:29 +0100

    token from Backebnd

--- src/SignIn.js
+++ src/SignIn.js
@@ -2,11 +2,32 @@ import React from 'react'
 import styled from "styled-components";
 import {Small,Body,Highlight,Subtitle,Title,Headings} from './Style/StyledTypography'
 import {Input,Button} from './Style/StyledComponents'
-function SignIn() {
+import axios from 'axios';
+
 
 const Container = styled.div`
 text-align: center;
 `
+
+function SignIn() {
+  const prevent = (e) =>{
+    console.log("it works")
+    // e.preventDefault()
+  }
+  const client = axios.create({
+    baseURL: 'http://localhost:3001',
+    headers: {
+      'Authorization': 'Bearer <YOUR_JWT_TOKEN>'
+    }
+  });
+
+  //TOKEN GET STORED IN LOCAL HOST WE RECIVE FROM BACKEND
+  const login = (email, password) =>
+  client.post('/login', { email:"laura@yourcompany.com", password:"asdasdasd" }).then(response => {
+    // Save the JWT token in local storage
+    localStorage.setItem('token', response.data.token);
+  });
+
   // function onSignIn(googleUser) {
   //   var profile = googleUser.getBasicProfile();
   //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -19,18 +40,19 @@ text-align: center;
     <div>SignIn</div>
     <Container>
       <Title>SignIn</Title>
-        <Input placeholder="Email"/>
-        <Input placeholder="Password"/>
-        <Button><Highlight>Sign In</Highlight></Button>
+        <form onSubmit={login}>
+        <Input type="email" name='email' placeholder="Email"/>
+        <Input type="password" name='password' placeholder="Password"/>
+        <Button type='submit'><Highlight>Sign In</Highlight></Button>
+        </form>
         <Small>Not a member? <span>Sign Up</span></Small>
       <Subtitle>Facebook</Subtitle>
-      <Subtitle>Google</Subtitle>
       <Subtitle>Apple</Subtitle>
       <Subtitle>Email</Subtitle>
 
+      {/* <div className="g-signin2" data-onsuccess="onSignIn"></div> */}
 
 
-    <div className="g-signin2" data-onsuccess="onSignIn"></div>
     </Container>
     </>
   )
