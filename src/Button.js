ed13250f6a76ca324b3007e40781bf885e463544
Quantumminded <lukkino31@gmail.com>
2022-12-20 11:33:51 +0100

    adding margin to buttons in nav bar

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className='bg-indigo-600 text-withe font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500 m-0.5'>
+    <button className='bg-indigo-600 text-withe font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500 mr-1'>
         {props.children}
     </button>
   )
