96baa0e55ce24d3b982a82f5cdb33854d01fc368
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-21 13:05:49 +0100

    Login/Signup

--- src/SignIn.js
+++ src/SignIn.js
@@ -25,55 +25,50 @@ function SignIn() {
   //Server needs to run on port 3001
   const [jwttoken, setJwttoken] = useState(localStorage.getItem("token"));
   const [signUp, setSignUp] = useState(false);
+  //Sent client for axios TODO:Work in progress
   const client = axios.create({
     baseURL: "http://localhost:3001",
     headers: {
       Authorization: `Bearer ${jwttoken}`,
     },
   });
+
+  //Sores the userInput of Login
   const email = useRef();
   const password = useRef();
 
+  //Handels the inputs
   function handleEmail(e) {
     email.current = e.target.value;
   }
   function handlePassword(e) {
     password.current = e.target.value;
-    setPassword2(e.target.value);
   }
-
-  const [password2, setPassword2] = useState();
 
   //TOKEN GET STORED IN LOCAL HOST WE RECIVE FROM BACKEND
   const login = (e, email, password) => {
     e.preventDefault();
     if (!signUp)
-      client.post("/login", { email, password }).then((response) => {
-        // Save the JWT token in local storage
-        localStorage.setItem("token", response.data.token);
-        setJwttoken(response.data.token);
-      });
-      // if(signUp)
-      // client.post("/signup", { email, password,firstName,lastName,languages,phonenumber }).then((response) => {
-      //   // Save the JWT token in local storage
-      //   localStorage.setItem("token", response.data.token);
-      //   setJwttoken(response.data.token);
-      // });
+      client
+        .post("/login", { email, password })
+        .then((response) => {
+          // Save the JWT token in local storage
+          setMessage(response.data.message);
+          localStorage.setItem("token", response.data.token);
+          setJwttoken(response.data.token);
+        })
+        .catch((err) => setMessage(err.response.data.message));
   };
 
-  // function onSignIn(googleUser) {
-  //   var profile = googleUser.getBasicProfile();
-  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  //   console.log('Name: ' + profile.getName());
-  //   console.log('Image URL: ' + profile.getImageUrl());
-  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  // }
+  const [message, setMessage] = useState()
+  //Form for Login
   if (!signUp)
     return (
       <>
         <div>SignIn</div>
         <Container>
           <Title>Login</Title>
+          {message}
           <form onSubmit={(e) => login(e, email.current, password.current)}>
             <Input
               onChange={(e) => handleEmail(e)}
@@ -93,51 +88,36 @@ function SignIn() {
             </Button>
           </form>
           <Small>
-            Not a member?{" "}
+            Not a member?
             <Span onClick={() => setSignUp(!signUp)}>Sign Up</Span>
           </Small>
           <Subtitle>Facebook</Subtitle>
           <Subtitle>Apple</Subtitle>
           <Subtitle>Email</Subtitle>
-
-          <div className="g-signin2" data-onsuccess="onSignIn"></div>
         </Container>
       </>
     );
+
+  //Form for SingUp
   if (signUp)
     return (
       <>
         <div>SignUp</div>
         <Container>
           <Title>SignUp</Title>
-          <form onSubmit={(e) => login(e, email.current, password.current)}>
-            <label for="firstName">Firstname</label>
+          <form action="http://localhost:3001/signup" method="POST">
             <Name type="text" name="firstName" placeholder="Firstname" />
             <Name type="text" name="lastName" placeholder="Lastname" />
-            <Input
-              onChange={(e) => handleEmail(e)}
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-            <Input
-              onChange={(e) => handlePassword(e)}
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
-            <Input
-              onChange={(e) => handlePassword(e)}
-              type="password"
-              placeholder="Confirm Password"
-            />
-            <AllLanguages/>
+            <Input type="email" name="email" placeholder="Email" />
+            <Input type="password" name="password" placeholder="Password" />
+            <Input type="password" placeholder="Confirm Password" />
+            <AllLanguages />
             <Button type="submit">
               <Highlight>Sign Up</Highlight>
             </Button>
           </form>
           <Small>
-            Already a member?{" "}
+            Already a member?
             <Span onClick={() => setSignUp(!signUp)}>Sign In</Span>
           </Small>
         </Container>
