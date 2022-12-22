f10c07484b1acdbf88fe4a79d1a5f4907616356c
Quantumminded <lukkino31@gmail.com>
2022-12-22 11:49:53 +0100

    adding herosection with button on top

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,18 +3,25 @@ import React from 'react'
 function HomePage() {
   return (
     <div className='bg-cyan-700'>
-      <div>
-        {/* Category button */}
-        <ul className='flex justify-around '>
-          <li className='m-2'><button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm gap-10"><a href='/'>Documents</a></button></li>
-          <li className='m-2'><button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm"><a href='/'>Calls</a></button></li>
-          <li className='m-2'><button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm"><a href='/'>Translations</a></button></li>
-        </ul>
-      </div>
-      {/* Header Image */}
-      <div className='flex justify-center'>
-        <img className='h-60 w-screen ml-8 mr-8 mt-3' src='https://images.unsplash.com/photo-1619418602850-35ad20aa1700?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80' alt='documents img'></img>
-      </div>
+    {/* Hero Section */}
+      <div className="hero min-h-min" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+        <div className="hero-overlay bg-opacity-60">
+          {/* Category Buttons */}
+          <ul className='flex justify-around'>
+            <li className='m-2'><button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm"><a href='/'>Documents</a></button></li>
+            <li className='m-2'><button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm"><a href='/Category'>Calls</a></button></li>
+            <li className='m-2'><button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm"><a href='/'>Translations</a></button></li>
+          </ul>
+        </div>
+        {/* Text Over Hero Img. */}
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md mt-10">
+              <h1 className="mb-5 text-5xl font-bold">Finde the perfect match !!!</h1>
+              <p className='mb-5'>Toffee powder toffee jelly sweet roll candy. Carrot cake biscuit candy canes lollipop sugar plum brownie. Danish sweet macaroon cupcake sesame snaps caramels pastry chocolate bar. Tart l</p>
+          </div>
+        </div>
+      </div> 
+      
       <div className='mt-8'>
         {/* H1 most popular in category */}
         <h1 className='ml-8 mr-8 text-xl'>Most popular in Documents</h1>
