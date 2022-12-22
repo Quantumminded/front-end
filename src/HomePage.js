474792c5085be627b8b5c3832063524d1886eb05
Quantumminded <lukkino31@gmail.com>
2022-12-22 23:12:22 +0100

    change card style calls category

--- src/HomePage.js
+++ src/HomePage.js
@@ -83,63 +83,7 @@ function HomePage() {
           </div>
         </div>
 
-        {/* Calls Container */}
-        <h2 className='mt-3 mb-3 text-2xl font-bold text-yellow-300'>Most popular in Calls</h2>
-        <div className='flex gap-10'>
-          {/* Calls Cards */}
-          <div className="card bg-yellow-400 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                Call
-                <div className="badge badge-secondary">NEW</div>
-              </h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">Call</div> 
-              </div>
-            </div>
-          </div>
-          <div className="card bg-yellow-400 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                Call
-                <div className="badge badge-secondary">NEW</div>
-              </h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">Call</div> 
-              </div>
-            </div>
-          </div>
-          <div className="card bg-yellow-400 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                Call
-                <div className="badge badge-secondary">NEW</div>
-              </h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">Call</div> 
-              </div>
-            </div>
-          </div>
-          <div className="card bg-yellow-400 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                Call
-                <div className="badge badge-secondary">NEW</div>
-              </h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">Call</div> 
-              </div>
-            </div>
-          </div>
-        </div>
+        
 
       {/* Translations Container */}
       <h2 className='mt-3 mb-3 text-2xl font-bold text-yellow-300'>Most popular in Translations</h2>
@@ -283,12 +227,131 @@ function HomePage() {
           </div>
         </div>
 			</div>
-      {/* End 4 card */}
-
-      
-        </div>    
-      </div>
     </div>
+    {/* End 4 card */}
+
+    {/* Start Calls container */}
+    <h2 className='mt-3 mb-3 text-2xl font-bold text-yellow-300'>Most popular in Calls</h2>
+     <div className='flex justify-center gap-5'>
+      {/* Start 1 card */}
+      <div className="wrapper bg-gray-400 antialiased text-gray-900 mb-10">
+        <div className='bg-cyan-700'>
+          <img src="https://source.unsplash.com/random/350x350" alt=" random imgee" className="w-full object-cover object-center rounded-lg shadow-md" />
+          <div className="relative px-4 -mt-16">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <div className="flex items-baseline">
+                <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
+                  Calls
+                </span>
+                <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                  Italian  &bull; German
+                </div>  
+              </div>
+              <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">Calls in German & italian</h4>
+              <div className="mt-1">
+                $100
+                <span className="text-gray-600 text-sm">  /h</span>
+              </div>
+              <div className="mt-4">
+                <span className="text-teal-600 text-md font-semibold">4/5 ratings </span>
+                <span className="text-sm text-gray-600">(based on 234 ratings)</span>
+              </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End 1 Card */}
+
+      {/* Start 2 card */}
+      <div className="wrapper bg-gray-400 antialiased text-gray-900 mb-10">
+        <div className='bg-cyan-700'>
+          <img src="https://source.unsplash.com/random/350x350" alt=" random imgee" className="w-full object-cover object-center rounded-lg shadow-md" />
+          <div className="relative px-4 -mt-16">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <div className="flex items-baseline">
+                <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
+                  Calls
+                </span>
+                <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                  Italian  &bull; German
+                </div>  
+              </div>
+              <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">Calls in German & italian</h4>
+              <div className="mt-1">
+                $100
+                <span className="text-gray-600 text-sm">  /h</span>
+              </div>
+              <div className="mt-4">
+                <span className="text-teal-600 text-md font-semibold">4/5 ratings </span>
+                <span className="text-sm text-gray-600">(based on 234 ratings)</span>
+              </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End 2 Card */}
+
+      {/* Start 3 card */}
+      <div className="wrapper bg-gray-400 antialiased text-gray-900 mb-10">
+        <div className='bg-cyan-700'>
+          <img src="https://source.unsplash.com/random/350x350" alt=" random imgee" className="w-full object-cover object-center rounded-lg shadow-md" />
+          <div className="relative px-4 -mt-16">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <div className="flex items-baseline">
+                <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
+                  Calls
+                </span>
+                <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                  Italian  &bull; German
+                </div>  
+              </div>
+              <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">Calls in German & italian</h4>
+              <div className="mt-1">
+                $100
+                <span className="text-gray-600 text-sm">  /h</span>
+              </div>
+              <div className="mt-4">
+                <span className="text-teal-600 text-md font-semibold">4/5 ratings </span>
+                <span className="text-sm text-gray-600">(based on 234 ratings)</span>
+              </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End 3 Card */}
+
+      {/* Start 4 card */}
+      <div className="wrapper bg-gray-400 antialiased text-gray-900 mb-10">
+        <div className='bg-cyan-700'>
+          <img src="https://source.unsplash.com/random/350x350" alt=" random imgee" className="w-full object-cover object-center rounded-lg shadow-md" />
+          <div className="relative px-4 -mt-16">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <div className="flex items-baseline">
+                <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
+                  Calls
+                </span>
+                <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                  Italian  &bull; German
+                </div>  
+              </div>
+              <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">Calls in German & italian</h4>
+              <div className="mt-1">
+                $100
+                <span className="text-gray-600 text-sm">  /h</span>
+              </div>
+              <div className="mt-4">
+                <span className="text-teal-600 text-md font-semibold">4/5 ratings </span>
+                <span className="text-sm text-gray-600">(based on 234 ratings)</span>
+              </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End 4 Card */}
+
+    </div> 
+  </div>
+</div>
            
   )
 }
