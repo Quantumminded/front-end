ca3185112ff6e34d25221f3f8016d951d491bf3a
Quantumminded <lukkino31@gmail.com>
2022-12-22 11:59:17 +0100

    adding button hover effect

--- src/HomePage.js
+++ src/HomePage.js
@@ -14,8 +14,8 @@ function HomePage() {
           </ul>
         </div>
         {/* Text Over Hero Img. */}
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md mt-10">
+        <div className="hero-content text-center text-neutral-content mt-20">
+          <div className="max-w-md ">
               <h1 className="mb-5 text-5xl font-bold">Finde the perfect match !!!</h1>
               <p className='mb-5'>Toffee powder toffee jelly sweet roll candy. Carrot cake biscuit candy canes lollipop sugar plum brownie. Danish sweet macaroon cupcake sesame snaps caramels pastry chocolate bar. Tart l</p>
           </div>
