78d4889f495fbe858be64cbf48d694a58c118cf7
Quantumminded <lukkino31@gmail.com>
2022-12-24 10:46:59 +0100

    change bg color and center the form in CallForm

--- src/CallForm.js
+++ src/CallForm.js
@@ -13,8 +13,8 @@ const CallForm = () => {
   //       // Place your API call here to submit your payload.
   //   };
     return (
-      <div className=''>
-      <div className='mx-auto'>
+      <div className='bg-b2'>
+      <div className=' pb-12 pt-12'>
         <form className=' ml-52 mr-52' id="login">
             <div className="bg-white dark:bg-gray-800">
                 <div className="container mx-auto bg-white dark:bg-gray-800 rounded">
@@ -190,7 +190,7 @@ const CallForm = () => {
                     </div>
                 </div>
                 
-                <div className="container mx-auto w-11/12 xl:w-full">
+                <div className=" container mx-auto w-11/12 xl:w-full">
                     <div className="w-full py-4 sm:px-0 bg-white dark:bg-gray-800 flex justify-end">
                         <button className="bg-gray-200 focus:outline-none transition duration-150 ease-in-out hover:bg-gray-300 dark:bg-gray-700 rounded text-indigo-600 dark:text-indigo-600 px-6 py-2 text-xs mr-4">Cancel</button>
                         <button className="bg-indigo-700 focus:outline-none transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-8 py-2 text-sm" type="submit">
