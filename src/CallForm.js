b41137075f4db08a3e0cc35efef9937af76a1a08
Quantumminded <lukkino31@gmail.com>
2022-12-25 16:54:45 +0100

    cenetring img and title in callForm

--- src/CallForm.js
+++ src/CallForm.js
@@ -14,13 +14,13 @@ const CallForm = () => {
   //   };
     return (
       <div className='bg-b2'>
-      <div className=' pb-12 pt-12'>
+      <div className='pb-12 pt-12'>
         <form className=' ml-52 mr-52' id="login">
             <div className="bg-white dark:bg-gray-800">
                 <div className="container mx-auto bg-white dark:bg-gray-800 rounded">
                     <div className="xl:w-full border-b border-gray-300 dark:border-gray-700 py-5 bg-white dark:bg-gray-800">
                         <div className="flex w-11/12 mx-auto xl:w-full xl:mx-0 items-center">
-                            <p className="text-lg text-gray-800 dark:text-gray-100 font-bold">Profile</p>
+                            <p className="mx-auto text-lg text-gray-800 dark:text-gray-100 font-bold">Profile</p>
                             <div className="ml-2 cursor-pointer text-gray-600 dark:text-gray-400">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={16} height={16}>
                                     <path className="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0v-4a1 1 0 0 1 1-1zm0-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" fill="currentColor" />
@@ -29,7 +29,7 @@ const CallForm = () => {
                         </div>
                     </div>
                     <div className="mx-auto">
-                        <div className="xl:w-9/12 w-11/12 mx-auto xl:mx-0">
+                        <div className="w-11/12 mx-auto ">
                             <div className="rounded relative mt-8 h-48">
                                 <img src="https://cdn.tuk.dev/assets/webapp/forms/form_layouts/form1.jpg" alt className="w-full h-full object-cover rounded absolute shadow" />
                                 <div className="absolute bg-black opacity-50 top-0 right-0 bottom-0 left-0 rounded" />
