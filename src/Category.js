b29776df37f396d0bdf19a226a2f3b7d1f7935e9
Quantumminded <lukkino31@gmail.com>
2023-01-01 12:21:23 +0100

    adding 4 rows cards in Catyegory.js

--- src/Category.js
+++ src/Category.js
@@ -9,9 +9,84 @@ function Category() {
   return (
     <div className='bg-b2'>
       <Banner/>
-            
-            
 
+
+      {/* Row 1  */}
+      <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pt-6 gap-8 p-5">
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+      </div>
+
+      {/* Row 2  */}
+      <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pt-6 gap-8 p-5">
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+      </div>
+
+      {/* Row 3  */}
+      <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pt-6 gap-8 p-5">
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        
+      </div>
+
+      {/* Row 4  */}
+      <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pt-6 gap-8 p-5">
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+        <div className="rounded border-gray-300">
+          <HomeCard />
+        </div>
+      </div>
 
     </div>
   )
