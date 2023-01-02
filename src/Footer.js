435743fdee81155879ab958291f04a3ec9793408
Quantumminded <lukkino31@gmail.com>
2023-01-02 11:51:27 +0100

    adding text to footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     
 
 <footer className="p-4 bg-b1 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span className="text-sm text-y1 sm:text-center dark:text-y1">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+    <span className="text-sm text-y1 sm:text-center dark:text-y1">© 2023 <a href="https://flowbite.com/" className="hover:underline">Website name™</a>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm text-y1 dark:text-gray-400 sm:mt-0">
         <li>
