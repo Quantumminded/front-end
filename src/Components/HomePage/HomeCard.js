a6ad4344a62a2a5488b7951afcacdfc519278200
Quantumminded <lukkino31@gmail.com>
2023-01-03 00:41:30 +0100

    adding flags to homecard

--- src/Components/HomePage/HomeCard.js
+++ src/Components/HomePage/HomeCard.js
@@ -5,11 +5,14 @@ function HomeCard() {
   return (
       <div className="wrapper bg-gray-400 antialiased text-gray-900 mb-10">
         <div className="bg-b2">
-          <img
-            src="https://source.unsplash.com/random/350x350"
-            alt=" random imgee"
-            className="w-full object-cover object-center rounded-lg shadow-md"
-          />
+        <div className="relative bottom-0 left-0">
+            <a className="absolute bottom-14 right-0 p-3 bg-transparent  animate-pulse">
+              <img className="p-2" src="https://flagcdn.com/24x18/de.png" alt="language" />
+              <img className="p-2" src="https://flagcdn.com/24x18/it.png" alt="language" />
+              <img className="p-2" src="https://flagcdn.com/24x18/gb-eng.png" alt="language" />
+            </a>
+            <img className="w-full shadow rounded" src="https://picsum.photos/400/300" alt="pic" />
+        </div>
           <div className="relative px-4 -mt-16">
             <div className="bg-white p-6 rounded-lg shadow-lg">
               <div className="flex items-baseline">
