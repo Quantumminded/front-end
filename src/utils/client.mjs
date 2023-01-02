4ab3928feb856612371bca1dc9e679f606d5b6ea
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-01-02 15:04:26 +0100

    token

--- src/utils/client.mjs
+++ src/utils/client.mjs
@@ -2,11 +2,9 @@ import axios from "axios";
 //  baseURL: "https://super-secret-backend.onrender.com/",
 //Get the Token in every call of the client("token in here").post() etc.
 const token = localStorage.getItem("token");
-const client = 
-  axios.create({
-    baseURL: "http://localhost:3001",
-    headers: { authorization: token },
-  });
-
+const client = axios.create({
+  baseURL: "https://super-secret-backend.onrender.com/",
+  headers: { authorization: token },
+});
 
 export { client };
