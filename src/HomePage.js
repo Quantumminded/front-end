68271c03f0c6c93e010d20902e7044c3d8a7ac66
Quantumminded <lukkino31@gmail.com>
2023-01-02 13:47:03 +0100

    add links to category buttons

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,7 +2,6 @@ import React from "react";
 
 // IMPORT COMPONENT
 import HomeCard from "./Components/HomePage/HomeCard";
-import HomeCard2 from "./Components/HomePage/HomeCard2";
 import HomeHeroSection from "./Components/HomePage/HomeHeroSection";
 
 import useCookies from "react-cookie/cjs/useCookies";
@@ -14,17 +13,17 @@ function HomePage() {
       <ul className="flex justify-around">
         <li className="m-2">
           <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm">
-            <a href="/">Documents</a>
+            <a href="/Category/documents">Documents</a>
           </button>
         </li>
         <li className="m-2">
           <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm">
-            <a href="/Category">Calls</a>
+            <a href="/Category/calls">Calls</a>
           </button>
         </li>
         <li className="m-2">
           <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm">
-            <a href="/">Translations</a>
+            <a href="/Category/translations">Translations</a>
           </button>
         </li>
       </ul>
