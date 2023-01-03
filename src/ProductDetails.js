692013ad6e54ffc8aafdaba2af0b78afd94bc5b5
Quantumminded <lukkino31@gmail.com>
2023-01-03 20:00:15 +0100

    adding description price and buttons to ProductDet

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -19,24 +19,6 @@ function ProductDetails() {
                 class="h-72 w-full rounded-xl object-cover lg:h-[540px]"
               />
 
-              <div class="absolute bottom-4 left-1/2 inline-flex -translate-x-1/2 items-center rounded-full bg-black/75 px-3 py-1.5 text-white">
-                <svg
-                  class="h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7"
-                  />
-                </svg>
-
-                <span class="ml-1.5 text-xs"> Hover to zoom </span>
-              </div>
             </div>
           </div>
 
@@ -65,7 +47,7 @@ function ProductDetails() {
               </fieldset>
 
               <fieldset>
-                <legend class="text-lg font-bold">Material</legend>
+                <legend class="text-lg font-bold">Type of documents</legend>
 
                 <div class="mt-2 flex gap-1">
                   <label for="material_cotton" class="cursor-pointer">
@@ -78,7 +60,7 @@ function ProductDetails() {
                     />
 
                     <span class="block rounded-full border border-gray-200 px-3 py-1 text-xs peer-checked:bg-gray-100">
-                      Cotton
+                      Bank
                     </span>
                   </label>
 
@@ -92,7 +74,7 @@ function ProductDetails() {
                     />
 
                     <span class="block rounded-full border border-gray-200 px-3 py-1 text-xs peer-checked:bg-gray-100">
-                      Wool
+                      Legal
                     </span>
                   </label>
                 </div>
@@ -100,7 +82,7 @@ function ProductDetails() {
 
               <div class="rounded border bg-gray-100 p-4">
                 <p class="text-sm">
-                  <span class="block"> Pay as low as $3/mo with 0% APR. </span>
+                  <span class="block">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
 
                   <a href="" class="mt-1 inline-block underline">
                     {" "}
@@ -110,7 +92,7 @@ function ProductDetails() {
               </div>
 
               <div>
-                <p class="text-xl font-bold">$19.99</p>
+                <p class="text-xl font-bold">$199.99/h</p>
               </div>
 
               <button
@@ -124,7 +106,7 @@ function ProductDetails() {
                 type="button"
                 class="w-full rounded border border-gray-300 bg-gray-100 px-6 py-3 text-sm font-bold uppercase tracking-wide"
               >
-                Notify when on sale
+                Book Now
               </button>
             </form>
           </div>
@@ -132,22 +114,20 @@ function ProductDetails() {
           <div class="lg:col-span-3">
             <div class="prose max-w-none [&>iframe]:mt-6 [&>iframe]:aspect-video [&>iframe]:w-full [&>iframe]:rounded-xl">
               <p>
+                DESCRIPTION <br></br>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
                 ad labore nostrum, a explicabo iste est dolorem deserunt id
                 ullam magni accusamus saepe, nulla sed sint reiciendis, aperiam
-                cumque officiis!
+                cumque officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+                eveniet ipsam mollitia nesciunt illo! Suscipit, corrupti!<br></br>
+                <br></br>
               </p>
 
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-                eveniet ipsam mollitia nesciunt illo! Suscipit, corrupti!
-              </p>
-
-              <h2>Features</h2>
+              <h2>ABOUT ME</h2>
 
               <ul>
-                <li>Smooth neck design</li>
-                <li>Breathable fabric</li>
+                <li>Professional translator</li>
+                <li>Details oriented</li>
                 <li>Odour prevention</li>
                 <li>Made from recycled materials</li>
               </ul>
