5ca749afd7af281a99aeec9ea9872105a56e7735
Quantumminded <lukkino31@gmail.com>
2023-01-05 02:50:24 +0100

    fetching data to documents most popular

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 // // // IMPORT COMPONENT
 import HomeCard from "./Components/HomePage/HomeCard";
+import Test from "./Components/HomePage/Test";
 import HomeHeroSection from "./Components/HomePage/HomeHeroSection";
 import CategoryButtons from "./Components/HomePage/CategoryButtons";
 
@@ -22,27 +23,8 @@ function HomePage() {
       {/* Documents cards section */}
       <h2 className="text-2xl font-bold tracking-tight text-gray-900 ml-7 mt-7">Most popular in Documents</h2>
       <div className="container mx-auto px-7 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pt-6 gap-8">
-                
-        <div className="rounded border-gray-300" >
-          <HomeCard />
-        </div>
-                
-        <div className="rounded border-gray-300" >
-          <HomeCard />
-        </div>
-
-        <div className="rounded border-gray-300" >
-          <HomeCard />
-        </div>
-
-        <div className="rounded border-gray-300" >
-          <HomeCard />
-        </div>
-
-      <div className="rounded border-gray-300" >
-        <HomeCard />
+        <Test />
       </div>
-    </div>
 
     {/* Calls cards section */}
     <h2 className="text-2xl font-bold tracking-tight text-gray-900 ml-7">Most popular in Calls</h2>
