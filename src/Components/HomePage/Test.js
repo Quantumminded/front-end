aa31ff262a75f9defafec6f04e8b9a99f23af904
Quantumminded <lukkino31@gmail.com>
2023-01-06 10:49:33 +0100

    adding new fecth http and fleg base on lenguage

--- src/Components/HomePage/Test.js
+++ src/Components/HomePage/Test.js
@@ -6,7 +6,7 @@ function Test({ language }) {
   
     useEffect(() => {
       async function fetchData() {
-        const res = await fetch("https://super-secret-backend.onrender.com/task/all");
+        const res = await fetch("https://super-secret-backend.onrender.com/api/task/all");
         const data = await res.json();
         setTasks(data);
       }
@@ -23,17 +23,17 @@ function Test({ language }) {
           <div className="bg-white ">
             <Link to="/ProductDetails">
               <div className="relative bottom-0 left-0 rounded-lg">
-                <div className="absolute bottom-14 right-0 p-3 bg-transparent  animate-pulse">
+                <div className="absolute bottom-14 right-0 p-3 bg-transparent animate-pulse">
                   {/* TEST FLEG  */}
-                {/* {task.languages.map((language) => (
+                {task.languages.map((language) => (
                   <img
                     key={language}
                     className="p-2"
                     src={`https://flagcdn.com/24x18/${language.toLowerCase()}.png`}
                     alt="language"
                   />
-                ))} */}
-                  <img
+                ))}
+                  {/* <img
                     className="p-2"
                     src="https://flagcdn.com/24x18/de.png"
                     alt="language"
@@ -47,7 +47,7 @@ function Test({ language }) {
                     className="p-2"
                     src="https://flagcdn.com/24x18/gb-eng.png"
                     alt="language"
-                  />
+                  /> */}
                 </div>
                 <img
                   className="w-full shadow rounded"
@@ -60,7 +60,7 @@ function Test({ language }) {
               <div className="bg-cyan-200 p-6 rounded-lg shadow-lg">
                 <div className="flex items-baseline">
                   <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
-                    Calls
+                    {task.type}
                   </span>
                   <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
                     {task.languages[0]} | {task.languages[1]}
--- src/HomePage.js
+++ src/HomePage.js
@@ -5,6 +5,7 @@ import React from "react";
 import HomeCard from "./Components/HomePage/HomeCard";
 import HomeHeroSection from "./Components/HomePage/HomeHeroSection";
 import CategoryButtons from "./Components/HomePage/CategoryButtons";
+import Test from "./Components/HomePage/Test";
 
 
 import useCookies from "react-cookie/cjs/useCookies";
@@ -21,7 +22,7 @@ function HomePage() {
       {/* Documents cards section */}
       <h2 className="text-2xl font-bold tracking-tight text-gray-900 ml-7 mt-7">Most popular in Documents</h2>
       <div className="container mx-auto px-7 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pt-6 gap-8">
-        <HomeCard />
+        <Test />
       </div>
 
     {/* Calls cards section */}
