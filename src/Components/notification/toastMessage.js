ff2b6c269af51806a7473eacb818a89a2287ec92
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-01-09 09:35:17 +0100

    toast comments

--- src/Components/notification/toastMessage.js
+++ src/Components/notification/toastMessage.js
@@ -1,8 +1,19 @@
 import { toast } from "react-toastify";
 
+const validTypes = ["success", "error", "warning", "info"];
+
 function toastMessage(type, message) {
   if (!message) return;
+
+  // Set default type to "info" if no type is provided
   if (!type) type = "info";
+
+  // Check if the provided type is valid
+  if (!validTypes.includes(type)) {
+    console.error(`"${type}" is not a valid type for toastMessage. Valid types are: ${validTypes.join(", ")}`);
+    return;
+  }
+
   toast[`${type}`](message, {
     position: "top-center",
     autoClose: 5000,
@@ -14,4 +25,5 @@ function toastMessage(type, message) {
     theme: "colored",
   });
 }
+
 export default toastMessage;
