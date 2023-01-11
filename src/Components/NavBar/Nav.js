3d47396371d5d0dc0f3045fada90802370159a85
Quantumminded <lukkino31@gmail.com>
2023-01-11 20:28:11 +0100

    deleting request button from navbar

--- src/Components/NavBar/Nav.js
+++ src/Components/NavBar/Nav.js
@@ -2,52 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NotificationCenter from "../notification/NotificationCenter";
 import ProfileIcon from "../notification/Profile/ProfileIcon";
-// import Button from "./Button";
 import "../../index.css";
 import useContextHook from "../../utils/customContextHook";
 
 function Nav() {
   const { authorized } = useContextHook();
-  // let Links = [
-  //   { name: "HOME", link: "/HomePage" },
-  //   { name: "SERVICES", link: "/" },
-  //   { name: "ABOUT", link: "/" },
-  //   { name: "CONTACT", link: "/" },
-  // ];
-  // let [open, setOpen] = useState(false);
   const [navbar, setNavbar] = useState(false);
   return (
-    // <nav className="shadow-md w-full fixed top-0 left-0">
-    //   <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
-    //     <div className="font-bold text-2xl cursor-pointer flex items-center text-grey-800">
-    //       <span className="text-3xl text-indigo-600 mr-1 pt-2">
-    //         <ion-icon name="logo-ionic"></ion-icon>
-    //       </span>
-    //       Name
-    //     </div>
-    //     <div
-    //       onClick={() => setOpen(!open)}
-    //       className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-    //     >
-    //       <ion-icon name={open ? "close" : "menu"}></ion-icon>
-    //     </div>
-    //     <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-i ${ open ? "top-20 opacity-100" : "top-[-490px]" } md:opacity-100`} >
-    //       {Links.map((link) => (
-    //         <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
-    //           <Link
-    //             to={link.link}
-    //             className="text-gray-800 hover:text-grey-400 duration-500"
-    //           >
-    //             {link.name}
-    //           </Link>
-    //         </li>
-    //       ))}
-    //       <Button>SignIn</Button>
-    //       <Button>Register</Button>
-    //     </ul>
-    //   </div>
-    // </nav>
-
     <nav className="w-full bg-b3 shadow">
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
@@ -113,19 +74,12 @@ function Nav() {
                 <Link to="/TeamSection">Contact US</Link>
               </li>
             </ul>
-
             <div className="mt-3 space-y-2 lg:hidden md:hidden">
               <Link
                 to="/SignIn"
-                className="inline-block w-full px-4 py-2 text-center text-b1 bg-yellow-300 rounded-md shadow hover:bg-gray-800"
+                className="inline-block w-full mb-1 px-4 py-2 text-center text-b1 bg-yellow-300 rounded-md shadow hover:bg-gray-800"
               >
                 Sign in
-              </Link>
-              <Link
-                to="/SelectRequest"
-                className="inline-block w-full px-4 py-2 text-center text-b1 bg-yellow-300 rounded-md shadow hover:bg-gray-100"
-              >
-                Post a request
               </Link>
             </div>
           </div>
