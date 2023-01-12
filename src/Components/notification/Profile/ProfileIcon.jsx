97daebf4c31b3171f84f5203489caf9a235e43d9
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-01-12 11:04:58 +0100

    ProfileIcon

--- src/Components/notification/Profile/ProfileIcon.jsx
+++ src/Components/notification/Profile/ProfileIcon.jsx
@@ -13,7 +13,7 @@ export default function ProfileIcon() {
                 <div className="flex flex-1">
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className="overflow-hidden relative w-12 h-12 rounded bg-gray-100 dark:bg-gray-600">
-                            <svg className=" w-12 h-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                            <svg className=" w-full h-full object-cover rounded absolute shadow flex items-center justify-center text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
                         </div>
                         <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
                             <li><Link to='/Profile'>Profile</Link></li>
@@ -30,7 +30,7 @@ export default function ProfileIcon() {
             <div className="flex flex-1 px-2">
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className="overflow-hidden relative w-12 h-12 rounded bg-gray-100 dark:bg-gray-600">
-                        <img className="w-12 h-12 rounded" src={user.image} alt="Default avatar" />                    </div>
+                        <img className="w-full h-full object-cover rounded absolute shadow flex items-center justify-center" src={user.image} alt="Default avatar" />                    </div>
                     <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
                         <li><Link to='/Profile'>Profile</Link></li>
                         <li><Link to='/Dashboard'>Dashboard</Link></li>
