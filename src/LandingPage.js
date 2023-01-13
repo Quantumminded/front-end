e78b97c3821f6da1328a83958e38cf8d0aee7dda
Quantumminded <lukkino31@gmail.com>
2023-01-13 10:32:25 +0100

    change card category colors and style in landing

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -29,35 +29,35 @@ function LandingPage() {
 
       {/* LANDING HERO SECTION */}
       {/* Documents hero  */}
-      <div className="dark:bg-gray-900">
+      <div className="">
         <div className="flex flex-col lg:flex-row items-stretch justify-between lg:px-0 px-6 lg:py-20 py-8 2xl:mx-auto 2xl:container">
           <div className="z-30 relative lg:w-1/2">
-            <div className="hidden dark:bg-gray-800 bg-y2 w-full lg:w-10/12 lg:h-full lg:flex justify-end items-center">
+            <div className="hidden rounded-3xl bg-y2 w-full lg:w-10/12 lg:h-full lg:flex justify-end items-center">
               {/* IMG */}
-              <div className="w-full lg:w-auto lg:-mr-32">
+              <div className="w-full lg:w-auto lg:-mr-32 ">
                 <img
                   src="https://i.ibb.co/YQjkqWr/juan-burgos-FIy-XGZ1q0mc-unsplash-1.png"
                   alt="with decent chairs"
-                  className="w-full relative z-30 lg:pl-20 px-6 py-14"
+                  className="w-full relative z-30 lg:pl-20 px-6 py-14 rounded-full"
                 />
               </div>
             </div>
-            <div className="absolute top-0 dark:bg-gray-800 bg-y2 md:h-96 w-full hidden md:block lg:hidden"></div>
-            <div className="w-full h-full lg:hidden bg-y2">
+            <div className="absolute top-0 bg-y2 md:h-96 w-full hidden md:block lg:hidden rounded-3xl"></div>
+            <div className="w-full h-full lg:hidden bg-y2 rounded-3xl">
               <img
                 src="https://i.ibb.co/YQjkqWr/juan-burgos-FIy-XGZ1q0mc-unsplash-1.png"
                 alt="with decent chairs"
-                className="w-full relative z-30 lg:pl-20 md:px-6 py-5 md:py-14"
+                className="w-full relative z-30 lg:pl-20 md:px-6 py-5 md:py-14 rounded-full"
               />
             </div>
           </div>
-          <div className="bg-y2 dark:bg-gray-800 lg:w-1/2 lg:ml-12 lg:p-14 p-8 flex items-center">
-            {/* Text & Button */}
+          {/* Text & Button */}
+          <div className=" rounded-3xl bg-b2 dark:bg-gray-800 lg:w-1/2 lg:ml-12 lg:p-14 p-8 flex items-center">
             <div>
-              <h1 className="dark:text-white md:w-8/12 lg:w-10/12 xl:8/12 2xl:w-8/12 w-full xl:text-6xl sm:text-5xl text-4xl font-semibold text-gray-800 capitalize">
+              <h1 className=" md:w-8/12 lg:w-10/12 xl:8/12 2xl:w-8/12 w-full xl:text-6xl sm:text-5xl text-4xl font-semibold text-y2 capitalize">
                 Documents
               </h1>
-              <p className="dark:text-gray-300 md:w-9/12 lg:w-11/12 xl:w-10/12 2xl:9/12 text-base leading-normal text-gray-600 mt-5">
+              <p className="dark:text-gray-300 md:w-9/12 lg:w-11/12 xl:w-10/12 2xl:9/12 text-base leading-normal text-white mt-5">
                 Get your documents translated with us! At [Company Name], we
                 specialize in providing high-quality and accurate translations
                 for a variety of personal and professional documents. From birth
@@ -107,35 +107,35 @@ function LandingPage() {
       </div>
 
       {/* Calls Hero  */}
-      <div className="dark:bg-gray-900">
+      <div className="">
         <div className="flex flex-col lg:flex-row items-stretch justify-between lg:px-0 px-6 lg:py-20 py-8 2xl:mx-auto 2xl:container">
           <div className="z-30 relative lg:w-1/2">
-            <div className="hidden dark:bg-gray-800 bg-y2 w-full lg:w-10/12 lg:h-full lg:flex justify-end items-center">
+            <div className="hidden rounded-3xl bg-b2 w-full lg:w-10/12 lg:h-full lg:flex justify-end items-center">
               {/* IMG */}
               <div className="w-full lg:w-auto lg:-mr-32">
                 <img
                   src="https://i.ibb.co/YQjkqWr/juan-burgos-FIy-XGZ1q0mc-unsplash-1.png"
                   alt="with decent chairs"
-                  className="w-full relative z-30 lg:pl-20 px-6 py-14"
+                  className="w-full relative z-30 lg:pl-20 px-6 py-14 rounded-full"
                 />
               </div>
             </div>
-            <div className="absolute top-0 dark:bg-gray-800 bg-y2 md:h-96 w-full hidden md:block lg:hidden"></div>
-            <div className="w-full h-full lg:hidden bg-y2">
+            <div className="absolute top-0 bg-b2 md:h-96 w-full hidden md:block lg:hidden rounded-3xl"></div>
+            <div className="w-full h-full lg:hidden bg-b2 rounded-3xl">
               <img
                 src="https://i.ibb.co/YQjkqWr/juan-burgos-FIy-XGZ1q0mc-unsplash-1.png"
                 alt="with decent chairs"
-                className="w-full relative z-30 lg:pl-20 md:px-6 py-5 md:py-14"
+                className="w-full relative z-30 lg:pl-20 md:px-6 py-5 md:py-14 rounded-full"
               />
             </div>
           </div>
-          <div className="bg-y2 dark:bg-gray-800 lg:w-1/2 lg:ml-12 lg:p-14 p-8 flex items-center">
-            {/* Text & Button */}
+          {/* Text & Button */}
+          <div className="rounded-3xl bg-y2 dark:bg-gray-800 lg:w-1/2 lg:ml-12 lg:p-14 p-8 flex items-center">
             <div>
-              <h1 className="dark:text-white md:w-8/12 lg:w-10/12 xl:8/12 2xl:w-8/12 w-full xl:text-6xl sm:text-5xl text-4xl font-semibold text-gray-800 capitalize">
+              <h1 className=" md:w-8/12 lg:w-10/12 xl:8/12 2xl:w-8/12 w-full xl:text-6xl sm:text-5xl text-4xl font-semibold text-b2 capitalize">
                 Calls
               </h1>
-              <p className="dark:text-gray-300 md:w-9/12 lg:w-11/12 xl:w-10/12 2xl:9/12 text-base leading-normal text-gray-600 mt-5">
+              <p className=" md:w-9/12 lg:w-11/12 xl:w-10/12 2xl:9/12 text-base leading-normal text-b3 mt-5">
                 At [Company Name], we understand the importance of clear and
                 accurate communication, especially when it comes to phone calls.
                 That's why we offer professional call translation services to
@@ -147,7 +147,7 @@ function LandingPage() {
                 ensure that your calls are seamless and accurately convey the
                 intended message.
               </p>
-              <button className=" sm:w-auto w-full mt-8 text-base justify-between focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none  font-medium leading-none  py-4 px-8 flex items-center text-b1 bg-y2 rounded-md shadow hover:bg-b3 hover:text-y2">
+              <button className=" sm:w-auto w-full mt-8 text-base justify-between focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none  font-medium leading-none  py-4 px-8 flex items-center text-y2 bg-b2 rounded-md shadow hover:bg-yellow-500 hover:text-b2">
                 <Link to="/Category/calls">Explore</Link>
                 <div className="ml-2 mt-0.5">
                   <svg
@@ -185,35 +185,35 @@ function LandingPage() {
       </div>
 
       {/* Translations Hero  */}
-      <div className="dark:bg-gray-900">
+      <div className="">
         <div className="flex flex-col lg:flex-row items-stretch justify-between lg:px-0 px-6 lg:py-20 py-8 2xl:mx-auto 2xl:container">
           <div className="z-30 relative lg:w-1/2">
-            <div className="hidden dark:bg-gray-800 bg-y2 w-full lg:w-10/12 lg:h-full lg:flex justify-end items-center">
+            <div className="rounded-3xl hidden bg-y2 w-full lg:w-10/12 lg:h-full lg:flex justify-end items-center">
               {/* IMG */}
               <div className="w-full lg:w-auto lg:-mr-32">
                 <img
                   src="https://i.ibb.co/YQjkqWr/juan-burgos-FIy-XGZ1q0mc-unsplash-1.png"
                   alt="with decent chairs"
-                  className="w-full relative z-30 lg:pl-20 px-6 py-14"
+                  className="w-full relative z-30 lg:pl-20 px-6 py-14 rounded-full"
                 />
               </div>
             </div>
-            <div className="absolute top-0 dark:bg-gray-800 bg-y2 md:h-96 w-full hidden md:block lg:hidden"></div>
-            <div className="w-full h-full lg:hidden bg-y2">
+            <div className="absolute top-0  bg-y2 md:h-96 w-full hidden md:block lg:hidden rounded-3xl"></div>
+            <div className="w-full h-full lg:hidden bg-y2 rounded-3xl">
               <img
                 src="https://i.ibb.co/YQjkqWr/juan-burgos-FIy-XGZ1q0mc-unsplash-1.png"
                 alt="with decent chairs"
-                className="w-full relative z-30 lg:pl-20 md:px-6 py-5 md:py-14"
+                className="w-full relative z-30 lg:pl-20 md:px-6 py-5 md:py-14 rounded-full"
               />
             </div>
           </div>
-          <div className="bg-y2 dark:bg-gray-800 lg:w-1/2 lg:ml-12 lg:p-14 p-8 flex items-center">
-            {/* Text & Button */}
+          {/* Text & Button */}
+          <div className="rounded-3xl bg-b2 lg:w-1/2 lg:ml-12 lg:p-14 p-8 flex items-center">
             <div>
-              <h1 className="dark:text-white md:w-8/12 lg:w-10/12 xl:8/12 2xl:w-8/12 w-full xl:text-6xl sm:text-5xl text-4xl font-semibold text-gray-800 capitalize">
+              <h1 className=" md:w-8/12 lg:w-10/12 xl:8/12 2xl:w-8/12 w-full xl:text-6xl sm:text-5xl text-4xl font-semibold text-y2 capitalize">
                 Translations
               </h1>
-              <p className="dark:text-gray-300 md:w-9/12 lg:w-11/12 xl:w-10/12 2xl:9/12 text-base leading-normal text-gray-600 mt-5">
+              <p className="md:w-9/12 lg:w-11/12 xl:w-10/12 2xl:9/12 text-base leading-normal text-white mt-5">
                 At [Company Name], we connect businesses and individuals with
                 skilled freelancer translators for all their translation needs.
                 Whether you need website localization, document translation, or
