738703d71ea46f5e5ed13df54a1ba51a06bd8994
Quantumminded <lukkino31@gmail.com>
2023-01-13 09:04:24 +0100

    changinc colors bg and buttons in navbar

--- src/Components/NavBar/Nav.js
+++ src/Components/NavBar/Nav.js
@@ -9,7 +9,7 @@ function Nav() {
   const { authorized } = useContextHook();
   const [navbar, setNavbar] = useState(false);
   return (
-    <nav className="w-full bg-b3 shadow">
+    <nav className="w-full bg-b2 shadow">
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
@@ -61,13 +61,13 @@ function Nav() {
             }`}
           >
             <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-              <li className="text-white hover:text-y1">
+              <li className="text-white hover:text-y2">
                 <Link to="/HomePage">Home</Link>
               </li>
-              <li className="text-white hover:text-y1">
+              <li className="text-white hover:text-y2">
                 <Link to="/">Services</Link>
               </li>
-              <li className="text-white hover:text-y1">
+              <li className="text-white hover:text-y2">
                 <Link to="/About">About US</Link>
               </li>
               <li className="text-white hover:text-y1">
@@ -76,10 +76,10 @@ function Nav() {
             </ul>
            {authorized ? (
             <>
-              <div className="pt-4">
+              <div className="sm:pt-3 ">
                 <Link
                   to="/Select"
-                  className="px-4 py-2 text-b1 bg-yellow-300 rounded-md shadow hover:bg-gray-800 hover:text-y1 self-center md:hidden lg:hidden sm:block">
+                  className="px-4 py-2 text-b1 bg-y2 rounded-md shadow hover:bg-gray-800 hover:text-y1 self-center md:hidden lg:hidden sm:block">
                   Get Started
                 </Link>
                 <div className=" md:hidden lg:hidden sm:flex sm:my-3 sm:space-x-2">
@@ -92,7 +92,7 @@ function Nav() {
             <div className="mt-3 space-y-2 lg:hidden md:hidden">
               <Link
                 to="/SignIn"
-                className="inline-block w-full mb-1 px-4 py-2 text-center text-b1 bg-yellow-300 rounded-md shadow hover:bg-gray-800"
+                className="inline-block w-full mb-1 px-4 py-2 text-center text-b1 bg-y1 rounded-md shadow hover:bg-b2 hover:text-y2 "
               >
                 Sign in
               </Link>
@@ -105,7 +105,7 @@ function Nav() {
           <div className="flex gap-4 ">
             <Link
               to="/Select"
-              className="hidden md:flex lg:flex px-4 py-2 text-b1 bg-yellow-300 rounded-md shadow hover:bg-gray-800 hover:text-y1 self-center "
+              className="hidden md:flex lg:flex px-4 py-2 text-b1 bg-y1 rounded-md shadow hover:bg-b2 hover:text-y2 self-center "
             >
               Get Started
             </Link>
