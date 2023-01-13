d45f9865b59c63f8b16791d9c2d55e4a64dabcbd
Quantumminded <lukkino31@gmail.com>
2023-01-13 13:58:55 +0100

    adding img to home page hero section

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <div className="bg-white">
       {/* Hero section */}
-      <div className="bg-gray-100">
+      <div className="bg-gray-100" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
         <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
           <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
             <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-b2 font-black leading-7 md:leading-10">
