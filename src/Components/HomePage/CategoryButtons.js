0d87fd5af1c060425c8c92ce930c336e6ef7c118
Quantumminded <lukkino31@gmail.com>
2023-01-17 23:18:30 +0100

    change category buttons

--- src/Components/HomePage/CategoryButtons.js
+++ src/Components/HomePage/CategoryButtons.js
@@ -3,23 +3,52 @@ import { Link } from "react-router-dom";
 
 function CategoryButtons() {
   return (
-    <ul className="flex justify-around bg-grey-100">
-         <li className="py-3">
-           <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm bg-b2">
-             <Link to="/Category/document">Documents</Link>
-           </button>
-         </li>
-         <li className="py-3">
-           <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm bg-b2">
-             <Link to="/Category/call">Calls</Link>
-           </button>
-         </li>
-         <li className="py-3">
-           <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm bg-b2">
-             <Link to="/Category/translation">Translations</Link>
-           </button>
-         </li>
-       </ul>
+    // <ul className="flex justify-around bg-grey-100">
+    //      <li className="py-3">
+    //        <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm bg-b2">
+    //          <Link to="/Category/document">Documents</Link>
+    //        </button>
+    //      </li>
+    //      <li className="py-3">
+    //        <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm bg-b2">
+    //          <Link to="/Category/call">Calls</Link>
+    //        </button>
+    //      </li>
+    //      <li className="py-3">
+    //        <button className="btn btn-outline btn-warning btn-xs sm:btn-sm md:btn-sm lg:btn-sm bg-b2">
+    //          <Link to="/Category/translation">Translations</Link>
+    //        </button>
+    //      </li>
+    //    </ul>
+
+    <div class="space-y-5">
+  <div class="border-b border-b-gray-200">
+    <ul class="-mb-px flex items-center gap-4 text-sm font-medium">
+      <li class="flex-1">
+        <Link to="/Category/call" className="relative flex items-center justify-center gap-2 px-1 py-3 text-b2 hover:text-y2">
+          Calls
+        </Link>
+      </li>
+      <li className="flex-1">
+        <Link to="/Category/document" className="flex items-center justify-center gap-2 px-1 py-3 text-b2 hover:text-y2">
+          Documents
+        </Link>
+      </li>
+      <li className="flex-1">
+        <Link to="/Category/translation" className="flex items-center justify-center gap-2 px-1 py-3 text-b2 hover:text-y2">
+          Translations
+        </Link>
+      </li>
+     
+      
+    </ul>
+  </div>
+  </div>
+
+
+
+
+
   )
 }
 
--- src/Components/HomePage/HomeHeroSection.js
+++ src/Components/HomePage/HomeHeroSection.js
@@ -3,12 +3,12 @@ import React from 'react'
 function HomeHeroSection() {
   return (
     <div className="hero min-h-min" style={{ backgroundImage: `url("https://www.atltranslate.com/hubfs/languages%20for%20translation.png")` }}>
-        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-overlay bg-opacity-70"></div>
         {/* Text Over Hero Img. */}
         <div className="hero-content text-center text-neutral-content py-16">
           <div className="max-w-md ">
               <h1 className="mb-5 text-5xl font-extrabold text-y1">Finde the perfect match !!!</h1>
-              <p className='mb-5 text-y1 font-bold'>Toffee powder toffee jelly sweet roll candy. Carrot cake biscuit candy canes lollipop sugar plum brownie. Danish sweet macaroon cupcake sesame snaps caramels pastry chocolate bar. Tart l</p>
+              <p className='mb-5 text-y1 font-bold'>We connect you with a network of expert freelance translators who specialize in a wide range of languages and industries. Our team of professional translators ensures that your documents, websites, and presentations are accurately translated to effectively communicate your message to your target audience.</p>
           </div>
         </div>
       </div> 
