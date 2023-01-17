8d066885d66bd518aad500e30fca8d21fbdd8d24
Quantumminded <lukkino31@gmail.com>
2023-01-17 12:23:00 +0100

    adding space to category button

--- src/Components/Category/Banner.js
+++ src/Components/Category/Banner.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Banner = () => {
     return (
-        <div className="hero min-h-min py-12 my-5  " style={{ backgroundImage: `url("https://www.compilatio.net/media/cache/adv_content_block_boxed/2020/11/4189-compilatio-translation.png")` }}>
+        <div className="hero min-h-min" style={{ backgroundImage: `url("https://www.compilatio.net/media/cache/adv_content_block_boxed/2020/11/4189-compilatio-translation.png")` }}>
         {/* Text Over Hero Img. */}
-        <div className="hero-content text-center text-neutral-content mt-20">
+        <div className="hero-content text-center text-neutral-content my-16">
           <div className="max-w-md ">
               <h1 className="mb-5 text-5xl font-bold text-b1">Finde the perfect match !!!</h1>
               <p className='mb-5 text-b1 font-bold'>Toffee powder toffee jelly sweet roll candy. Carrot cake biscuit candy canes lollipop sugar plum brownie. Danish sweet macaroon cupcake sesame snaps caramels pastry chocolate bar. Tart l</p>
