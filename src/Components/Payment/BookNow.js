c57617789895515c1d6652743cbd5d9b914a5b7a
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-01-18 16:22:42 +0100

    railway

--- src/Components/Payment/BookNow.js
+++ src/Components/Payment/BookNow.js
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function BookNow({ card }) {
-  console.log(card)
-    const [error, setError] = useState(null);
+  console.log(card);
+  const [error, setError] = useState(null);
 
   const handleClick = (card) => {
-    fetch("http://localhost:3001/create-checkout-session", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        items: [
-          { id: 1, quantity: 3 },
-          { id: 2, quantity: 1 },
-        ],
-        
-      }),
-    })
-      .then(res => {
+    fetch(
+      "https://ultra-top-secret-backend-production.up.railway.app/create-checkout-session",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          items: [
+            { id: 1, quantity: 3 },
+            { id: 2, quantity: 1 },
+          ],
+        }),
+      }
+    )
+      .then((res) => {
         if (!res.ok) {
-          return res.json().then(json => {
+          return res.json().then((json) => {
             throw new Error(json.error);
           });
         }
@@ -29,16 +31,16 @@ function BookNow({ card }) {
       .then(({ url }) => {
         window.location = url;
       })
-      .catch(error => {
+      .catch((error) => {
         setError(error);
       });
-  }
+  };
   return (
     <div className="h-12 w-40 text-center float-left bg-y2 hover:bg-b2 text-b2 hover:text-y2 text-xl font-bold p-2 m-2 rounded-full -mt-2 shadow-lg">
       <button onClick={() => handleClick(card)}>Checkout</button>
       {error && <p>{error.message}</p>}
     </div>
-  )
+  );
 }
 
-export default BookNow
+export default BookNow;
